64f0022000838c7c2f4d37d99dc1ae55f78bedbd
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-04-23 18:27:48 -0700

    added control bar

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function over(video) {
-    video.play();
+var city = document.querySelector(".city")
+
+function over() {
+    city.play();
 }
-function out(video) {
-    video.pause();
+function out() {
+    city.pause();
+}
+
+function fastVid() {
+    city.playbackRate = 5;
+}
+function rw2() {
+    city.currentTime -= 2.5;
+}
+function fw2(){
+    city.currentTime += 2.5;
 }
