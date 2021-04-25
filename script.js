ad564ef7ee144662e17711807c5783b22f73e0cc
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-04-24 19:07:48 -0700

    added better functionality to fast forward

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 var city = document.querySelector(".city")
-
+var isPlaying = false;
 function over() {
     city.play();
+    isPlaying = true;
 }
 function out() {
     city.pause();
+    isPlaying = false;
 }
 
 function fastVid() {
+    if(isPlaying) {
     city.playbackRate = 5;
+    }
 }
 function rw2() {
     city.currentTime -= 2.5;
+    city.playbackRate = 1;
 }
 function fw2(){
     city.currentTime += 2.5;
